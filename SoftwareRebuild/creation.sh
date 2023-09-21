fda1640c0950d6241607901244ebe6b1c3f72174
3drefuge <114976560+3drefuge@users.noreply.github.com>
2023-09-21 03:53:11 -0400

    Update creation.sh
    Converted from .sh to .vbs so we can launch shortcuts from Windows and network drives.

--- SoftwareRebuild/creation.sh
+++ SoftwareRebuild/creation.sh
@@ -43,28 +43,28 @@ for machine_group in "${!machines[@]}"; do
     folder_name="$machine_name/${machine_name%?}$i"
     create_folders "$folder_name"
     
-    # Create "ON.sh" with specific content
-    on_script_content="#!/bin/bash\necho \"run.sh smarton $(basename "$folder_name")\" | timeout 0.1 nc -v 192.168.77.120 12345"
-    create_machine_script "$folder_name" "$machine_name" "ON.sh" "$on_script_content"
+    # Create "ON.vbs"
+    on_script_content="Set WshShell = CreateObject(\"WScript.Shell\") : WshShell.Run \"cmd /c echo run.sh smarton $(basename "$folder_name") | nc.exe -w 1 -v 192.168.77.120 12345\", 0 : MsgBox \"Command OK!\", vbInformation, \"DoDo3D Commander v1.0\""
+    create_machine_script "$folder_name" "$machine_name" "ON.vbs" "$on_script_content"
 
-    # Create "OFF.sh" with different content
-    off_script_content="#!/bin/bash\necho \"run.sh smartoff $(basename "$folder_name")\" | timeout 0.1 nc -v 192.168.77.120 12345"
-    create_machine_script "$folder_name" "$machine_name" "OFF.sh" "$off_script_content"
+    # Create "OFF.vbs"
+    off_script_content="Set WshShell = CreateObject(\"WScript.Shell\") : WshShell.Run \"cmd /c echo run.sh smartoff $(basename "$folder_name") | nc.exe -w 1 -v 192.168.77.120 12345\", 0 : MsgBox \"Command OK!\", vbInformation, \"DoDo3D Commander v1.0\""
+    create_machine_script "$folder_name" "$machine_name" "OFF.vbs" "$off_script_content"
 
-    # Create "CANCEL.sh" with "cancelJOB" content
-    cancel_script_content="#!/bin/bash\necho \"run.sh canceljob $(basename "$folder_name")\" | timeout 0.1 nc -v 192.168.77.120 12345"
-    create_machine_script "$folder_name" "$machine_name" "CANCEL.sh" "$cancel_script_content"
+    # Create "CANCEL.vbs"
+    cancel_script_content="Set WshShell = CreateObject(\"WScript.Shell\") : WshShell.Run \"cmd /c echo run.sh canceljob $(basename "$folder_name") | nc.exe -w 1 -v 192.168.77.120 12345\", 0 : MsgBox \"Command OK!\", vbInformation, \"DoDo3D Commander v1.0\""
+    create_machine_script "$folder_name" "$machine_name" "CANCEL.vbs" "$cancel_script_content"
 
-    # Create "UNLOAD.sh" with "unload" content
-    unload_script_content="#!/bin/bash\necho \"run.sh unload $(basename "$folder_name")\" | timeout 0.1 nc -v 192.168.77.120 12345"
-    create_machine_script "$folder_name" "$machine_name" "UNLOAD.sh" "$unload_script_content"
+    # Create "UNLOAD.vbs"
+    unload_script_content="Set WshShell = CreateObject(\"WScript.Shell\") : WshShell.Run \"cmd /c echo run.sh unload $(basename "$folder_name") | nc.exe -w 1 -v 192.168.77.120 12345\", 0 : MsgBox \"Command OK!\", vbInformation, \"DoDo3D Commander v1.0\""
+    create_machine_script "$folder_name" "$machine_name" "UNLOAD.vbs" "$unload_script_content"
 
-    # Create "REPRINT.sh" with "unload" content
-    reprint_script_content="#!/bin/bash\necho \"run.sh reprint $(basename "$folder_name")\" | timeout 0.1 nc -v 192.168.77.120 12345"
-    create_machine_script "$folder_name" "$machine_name" "REPRINT.sh" "$reprint_script_content"
+    # Create "REPRINT.vbs"
+    reprint_script_content="Set WshShell = CreateObject(\"WScript.Shell\") : WshShell.Run \"cmd /c echo run.sh reprint $(basename "$folder_name") | nc.exe -w 1 -v 192.168.77.120 12345\", 0 : MsgBox \"Command OK!\", vbInformation, \"DoDo3D Commander v1.0\""
+    create_machine_script "$folder_name" "$machine_name" "REPRINT.vbs" "$reprint_script_content"
 
-    # Create "PIDTUNE.sh" with "unload" content
-    pidtune_script_content="#!/bin/bash\necho \"run.sh pidtune $(basename "$folder_name")\" | timeout 0.1 nc -v 192.168.77.120 12345"
-    create_machine_script "$folder_name" "$machine_name" "PIDTUNE.sh" "$reprint_script_content"
+    # Create "PIDTUNE.vbs"
+    pidtune_script_content="Set WshShell = CreateObject(\"WScript.Shell\") : WshShell.Run \"cmd /c echo run.sh pidtune $(basename "$folder_name") | nc.exe -w 1 -v 192.168.77.120 12345\", 0 : MsgBox \"Command OK!\", vbInformation, \"DoDo3D Commander v1.0\""
+    create_machine_script "$folder_name" "$machine_name" "PIDTUNE.vbs" "$pidtune_script_content"
   done
 done
